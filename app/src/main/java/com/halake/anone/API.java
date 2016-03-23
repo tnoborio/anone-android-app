00cbddb804aa11b65ba790a3507d759b6425df40
Tokusei Noborio <tokusei@nyampass.com>
2016-03-23 12:18:07 +0900

    Implement to post stamp api

--- app/src/main/java/com/halake/anone/API.java
+++ app/src/main/java/com/halake/anone/API.java
@@ -1,7 +1,25 @@
 package com.halake.anone;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.halake.anone.models.Message;
+import com.halake.anone.models.Utils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -11,11 +29,126 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 public class API {
+    private static RequestQueue requestQueue;
+    private static String BASE_URL = "http://13.71.156.156:4567/api";
+
+    public static void setup(Context context) {
+        requestQueue = Volley.newRequestQueue(context);
+    }
+
+    public static class FetchMessages extends JsonArrayRequest {
+        public FetchMessages(Response.Listener<JSONArray> listener,
+                          Response.ErrorListener errorListener, int limit) {
+            super(BASE_URL + "/papa/messages?limit=" + limit, listener, errorListener);
+        }
+
+        interface Listener {
+            void onSuccess(List<Message> messages);
+            void onError();
+        }
+    }
+
+    public static void fetchMessages(final FetchMessages.Listener listener) {
+        requestQueue.add(new FetchMessages(new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray result) {
+                listener.onSuccess(Message.json2List(result));
+                Log.d("", result.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                listener.onError();
+            }
+        }, 10));
+    }
+
+    public static class PostStamp extends JsonObjectRequest {
+        private static JSONObject request(String to) {
+            JSONObject obj = new JSONObject();
+            try {
+                obj.put("to", to);
+            } catch (JSONException e) {
+                /* ignore */
+            }
+            return obj;
+        }
+
+        public PostStamp(final Listener listener, String to, final Bitmap stampBitmap) {
+            super(Method.POST, BASE_URL + "/papa/stamps", request(to), new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject obj) {
+                    String path;
+                    try {
+                        path = obj.getString("post_path");
+                    } catch (JSONException e) {
+                        listener.onError();
+                        return;
+                    }
+                    requestQueue.add(new FileRequest(path, listener, Utils.bitmapBytes(stampBitmap)));
+                }
+
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    listener.onError();
+                }
+            });
+        }
+
+        private static class FileRequest extends Request<String> {
+            private byte[] body;
+            private Listener listener;
+
+            public FileRequest(String url, final Listener listener, byte[] body) {
+                super(Method.POST, url, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        listener.onError();
+                    }
+                });
+                this.listener = listener;
+                this.body = body;
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/octet-stream";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                return this.body;
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                return Response.success("Uploaded", getCacheEntry());
+
+            }
+
+            @Override
+            protected void deliverResponse(String response) {
+                this.listener.onSuccess();
+            }
+        }
+
+        interface Listener {
+            void onSuccess();
+            void onError();
+        }
+    }
+
+    public static void postStamp(final Bitmap bitmap, String to, final PostStamp.Listener listener) {
+        requestQueue.add(new PostStamp(listener, to, bitmap));
+    }
+
     public static void postToken(String token) {
         try {
-            HttpURLConnection conn = (HttpURLConnection) new URL("http://13.71.156.156:4567/api/papa/token/android").openConnection();
+            HttpURLConnection conn = (HttpURLConnection) new URL(BASE_URL + "/papa/token/android").openConnection();
 
             conn.setReadTimeout(10000);
             conn.setConnectTimeout(15000);
@@ -39,7 +172,7 @@ public class API {
 
             conn.connect();
 
-            StringBuffer response = new StringBuffer();
+            StringBuilder response = new StringBuilder();
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line;
